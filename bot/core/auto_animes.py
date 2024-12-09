d71106646ad3ac33c76664a7287cb48f42e8ee92
YaexMiko <aloksinghmains756@gmail.com>
2024-12-09 09:17:16 +0530

    Update auto_animes.py

--- bot/core/auto_animes.py
+++ bot/core/auto_animes.py
@@ -18,10 +18,10 @@ from .tguploader import TgUploader
 from .reporter import rep
 
 btn_formatter = {
-    '1080':'𝟭𝟬𝟴𝟬𝗽', 
-    '720':'𝟳𝟮𝟬𝗽',
+    '1080':'𝟭𝟬𝟴𝟬𝗽',
     '360':'360p',
     '480':'𝟰𝟴𝟬𝗽',
+    '720':'𝟳𝟮𝟬𝗽',
     }
 
 async def fetch_animes():
