13a2adbab01e9d8e1239451686927c0e2ef0d342
YaexMiko <aloksinghmains756@gmail.com>
2025-01-08 20:39:43 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -41,11 +41,11 @@ class Var:
 
     RSS_ITEMS = getenv("RSS_ITEMS", "https://subsplease.org/rss/?r=1080").split()
     FSUB_CHATS = list(map(int, getenv('FSUB_CHATS').split()))
-    BACKUP_CHANNEL = getenv("BACKUP_CHANNEL") or ""
-    MAIN_CHANNEL = int(getenv("MAIN_CHANNEL"))
+    BACKUP_CHANNEL = getenv("BACKUP_CHANNEL", "-1002392729611") or ""
+    MAIN_CHANNEL = int(getenv("MAIN_CHANNEL", "-1002289975824"))
     LOG_CHANNEL = int(getenv("LOG_CHANNEL") or 0)
-    FILE_STORE = int(getenv("FILE_STORE"))
-    ADMINS = list(map(int, getenv("ADMINS", "1242011540").split()))
+    FILE_STORE = int(getenv("FILE_STORE", "-1002329208995"))
+    ADMINS = list(map(int, getenv("ADMINS", "8108281129").split()))
     
     SEND_SCHEDULE = getenv("SEND_SCHEDULE", "False").lower() == "true"
     BRAND_UNAME = getenv("BRAND_UNAME", "@username")
